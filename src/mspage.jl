4bbfdb742213339351d687b8022388887d94186a
Neel Smith <dnsmith.neel@gmail.com>
2023-07-27 08:14:41 -0400

    save page bounds with page data

--- src/mspage.jl
+++ src/mspage.jl
@@ -2,6 +2,7 @@
 """
 struct MSPage
     pageurn::Cite2Urn
+    pagebounds::Cite2Urn
     iliadlines::Vector{CtsUrn}
     scholia::Vector{ScholionIliadPair}
     imagezone::Cite2Urn
@@ -12,54 +13,67 @@ struct MSPage
 end
 
 
+"""Collect all information needed to analyze the
+layout of a page.
+$(SIGNATURES)
+"""
 function msPage(pageurn::Cite2Urn; data = nothing)::Union{MSPage, Nothing}
     hmtdata = isnothing(data) ? hmt_cex() : data
-    dse = hmt_dse(hmtdata)[1] 
-    textpassages = textsforsurface(pageurn, dse)
+    boundsimgs = filter(hmt_pagerois(hmtdata).index) do pr
+        string(pr[1]) == string(pageurn)
+    end
+    if length(boundsimgs) != 1
+        throw(ArgumentError("$(pageurn) does not have indexed page boundaries in the HMT archive"))
 
-    iliadreff =  filter(u -> startswith(workcomponent(u), "tlg0012.tlg001."), textpassages)
-    iliadstrings = map(u -> string(u),iliadreff)
+    else
+        boundsimg = boundsimgs[1][2]
+        dse = hmt_dse(hmtdata)[1] 
+        textpassages = textsforsurface(pageurn, dse)
 
-    allcommentary = hmt_commentary(hmtdata)[1]
-    
-    scholiareff = filter(psg -> startswith(workcomponent(psg), "tlg5026"), textpassages)
-    scholiapairs = map(scholiareff) do s
-        iliadmatches = filter(pr -> pr[1] == s, allcommentary.commentary)
-        if length(iliadmatches) == 1
-            iliad = iliadmatches[1][2]
-            lineindex = findfirst(isequal(string(iliad)), iliadstrings)
+        iliadreff =  filter(u -> startswith(workcomponent(u), "tlg0012.tlg001."), textpassages)
+        iliadstrings = map(u -> string(u),iliadreff)
 
-            schimagematches = imagesfortext(s, dse)
-            ilimagematches = imagesfortext(iliad, dse)
+        allcommentary = hmt_commentary(hmtdata)[1]
+        
+        scholiareff = filter(psg -> startswith(workcomponent(psg), "tlg5026"), textpassages)
+        scholiapairs = map(scholiareff) do s
+            iliadmatches = filter(pr -> pr[1] == s, allcommentary.commentary)
+            if length(iliadmatches) == 1
+                iliad = iliadmatches[1][2]
+                lineindex = findfirst(isequal(string(iliad)), iliadstrings)
 
-            if length(schimagematches) == 1 && length(ilimagematches) == 1
-                ScholionIliadPair(
-                    s,
-                    schimagematches[1],
-                    iliad,
-                    ilimagematches[1],
-                    lineindex
-                )
+                schimagematches = imagesfortext(s, dse)
+                ilimagematches = imagesfortext(iliad, dse)
 
+                if length(schimagematches) == 1 && length(ilimagematches) == 1
+                    ScholionIliadPair(
+                        s,
+                        schimagematches[1],
+                        iliad,
+                        ilimagematches[1],
+                        lineindex
+                    )
+
+                else
+                    @warn("Failed to find images for scholion $(s) and Iliad line $(iliad).")
+                    nothing
+                end
             else
-                @warn("Failed to find images for scholion $(s) and Iliad line $(iliad).")
+                @warn("No Iliad match for scholion $(s).")
                 nothing
             end
-        else
-            @warn("No Iliad match for scholion $(s).")
-            nothing
         end
-    end
-
-
-   
-
 
 
     
-    izone = Cite2Urn("urn:cite2:fake:no.no:")
-    side = nothing
-    topthresh = nothing
-    bottomthresh = nothing
-    MSPage(pageurn, iliadreff, scholiapairs, izone, side, topthresh, bottomthresh)
+
+
+
+        
+        izone = Cite2Urn("urn:cite2:fake:no.no:")
+        side = nothing
+        topthresh = nothing
+        bottomthresh = nothing
+        MSPage(pageurn, boundsimg, iliadreff, scholiapairs, izone, side, topthresh, bottomthresh)
+    end
 end
--- test/test_mspage.jl
+++ test/test_mspage.jl
@@ -1,4 +1,5 @@
 @testset "Test collecting page data" begin
     pageurn = Cite2Urn("urn:cite2:hmt:msA.v1:55r")
     pg = msPage(pageurn)
+    @test pg isa MSPage
 end
