2ee9b13cda743395fb840050e794e48c9bbb00a7
Neel Smith <dnsmith.neel@gmail.com>
2023-07-27 14:50:32 -0400

    Pass initial limited unit tests.

--- test/test_mspage.jl
+++ test/test_mspage.jl
@@ -9,7 +9,7 @@
     @test pg_bbox == (left = 0.144, top = 0.213, right = 0.545, bottom = 0.697)
 
     # Test a page with no page bounding box
-    @test isnothing(Cite2Urn("urn:cite2:hmt:msA.v1:11v") )
+    @test msPage(Cite2Urn("urn:cite2:hmt:msA.v1:11v"); data = src) |> isnothing
     # Test a page with no scholia (Bessarion replacement page)
     noscholiapg = msPage(Cite2Urn("urn:cite2:hmt:msA.v1:69r"); data = src)
     @test noscholiapg isa MSPage
