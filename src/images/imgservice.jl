38ed7ba0d98b2fc35a7681641998707e35e1370e
Neel Smith <dnsmith.neel@gmail.com>
2023-07-28 14:11:20 -0400

    Pass unit tests with new pkg versions

--- src/images/imgservice.jl
+++ src/images/imgservice.jl
@@ -12,9 +12,9 @@ end
 a matrix of RGB values.
 $(SIGNATURES)
 """
-function load_rgb(imgurn::Cite2Urn)::Matrix{RGB{N0f8}}
+function load_rgb(imgurn::Cite2Urn; w = 800)::Matrix{RGB{N0f8}}
     trimmed = dropsubref(imgurn)
-	iifrequest = url(trimmed, imgservice())
+	iifrequest = url(trimmed, imgservice(); w = w)
     f = Downloads.download(iifrequest)
     img = load(f)
 	rm(f)
@@ -25,29 +25,8 @@ end
 Returns a matrix of RGBA values.
 $(SIGNATURES)
 """
-function load_rgba(imgurn::Cite2Urn; alpha = 1.0)::Matrix{RGB{N0f8}}
-	RGBA.(load_rgb(imgurn), alpha)
+function load_rgba(imgurn::Cite2Urn; w = 800, alpha = 1.0)::Matrix{RGBA{N0f8}}
+    img = load_rgb(imgurn; w = w)
+	RGBA.(img, alpha)
 end
 
-
-"""Extract region-of-interest string from an image URN, and convert to a vector of floats rounded to `digits` significant places.
-It is an error if the syntax of the URN's subreference is invalid.
-$(SIGNATURES)
-"""
-function imagefloats(imgu::Cite2Urn; digits = 3)
-    if hassubref(imgu)
-        
-        parts = split(subref(imgu),",")
-        @debug("Find floats for subreference with parts $(parts)")
-        if length(parts) == 4
-            floats = map(roi -> parse(Float64, roi), parts)
-            rounded = map(num -> round(num, digits=digits), floats)
-            (left = rounded[1], top = rounded[2], width = rounded[3], height = rounded[4])
-        else
-            throw(ArgumentError("""Invalid region of interest in urn `$(imgu)`:  expression must have four comma-separated parts.""")) 
-        end
-
-    else
-        nothing
-    end
-end
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,7 +1,7 @@
 @testset "Test extracting data from image URNs" begin
     goodurn = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA055RN_0056@0.065,0.0695,0.8188,0.8704")
-    @test MiseEnPage.imagefloats(goodurn) == [0.065, 0.07, 0.819, 0.87]
-    @test MiseEnPage.imagefloats(goodurn; digits = 2) == [0.06, 0.07, 0.82, 0.87]
+    @test MiseEnPage.imagefloats(goodurn) == (left = 0.065, top = 0.07, width = 0.819, height = 0.87)
+    @test MiseEnPage.imagefloats(goodurn; digits = 2) == (left = 0.06, top = 0.07, width = 0.82, height = 0.87)
 
 
     nosubref = Cite2Urn("urn:cite2:hmt:vaimg.2017a:VA055RN_0056")
