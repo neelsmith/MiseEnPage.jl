94f34a0a69d5c6d46094db184667c7f3c77c342d
Neel Smith <dnsmith.neel@gmail.com>
2023-07-31 12:09:41 -0400

    Zone boxing

--- scripts/diagram-parts.jl
+++ scripts/diagram-parts.jl
@@ -8,26 +8,20 @@ img = load_rgba(mspage, w = 400)
 dimm = dimensions(img)
 
 
-topbox = MiseEnPage.topzone_box()
-bottombox = MiseEnPage.bottomzone_box()
-sidebox = MiseEnPage.adjacentzone_box()
-
-function boxer(boxcoords, pagedimm)
-    lt = Point(boxcoords[:left] * pagedimm[:w], boxcoords[:top] * pagedimm[:h])
-    rb = Point(boxcoords[:left] * pagedimm[:w] + boxcoords[:width] * pagedimm[:w], boxcoords[:top]*pagedimm[:h] + boxcoords[:height]*pagedimm[:h])
-    
-    box(lt, rb, :stroke)
-end
-
 @draw begin
     translate(-1 * dimm[:w]/2,  -1 * dimm[:h]/2)
     placeimage(img,O)
-    sethue("goldenrod2")
-    boxer(topbox, dimm)
-    boxer(bottombox, dimm)
-    sethue("orchid")
-    boxer(sidebox, dimm)
 
-    
-    
+    setline(2)
+    sethue("lightblue3")
+    setdash("dot")
+    pagebox_luxor(mspage, img)
+
+    sethue("gray81")
+    setdash("solid")
+    setline(2)
+    iliadbox_luxor(mspage, img)
+
+    sethue("gainsboro")
+    zonesboxed_luxor(mspage, img)
 end dimm[:w] dimm[:h]
